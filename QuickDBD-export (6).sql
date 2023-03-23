c1b85cdf7625e9cb0cd8a1342a0b8fbb3de8ae22
christophercruz0@gmail.com <ChristopherCruz0@gmail.com>
2023-03-23 19:43:50 -0400

    Update

---
+++ QuickDBD-export (6).sql
@@ -0,0 +1,68 @@
+﻿
+
+CREATE TABLE "contacts" (
+    "contact_id_num" INTEGER   NOT NULL,
+    "first_name" VARCHAR(50)   NOT NULL,
+    "last_name" VARCHAR(50)   NOT NULL,
+    "email" VARCHAR(150),
+    CONSTRAINT "pk_Contacts" PRIMARY KEY (
+        "contact_id_num"
+     )
+);
+
+CREATE TABLE "campaign" (
+    "cf_id" INTEGER   NOT NULL,
+    "contact_id" INTEGER   NOT NULL,
+    "company_name" VARCHAR(200)   NOT NULL,
+    "description" VARCHAR(300)   NOT NULL,
+    "goal" FLOAT   NOT NULL,
+    "pledged" FLOAT   NOT NULL,
+    "outcome" VARCHAR(50)   NOT NULL,
+    "backers_count" INTEGER   NOT NULL,
+    "country" VARCHAR(4)   NOT NULL,
+    "currency" VARCHAR(6)   NOT NULL,
+    "launched_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "staff_pick" VARCHAR(8)   NOT NULL,
+    "spotlight" VARCHAR(8)   NOT NULL,
+    "category" VARCHAR(15)   NOT NULL,
+    "subcategory" VARCHAR(50)   NOT NULL,
+    "category_id" VARCHAR(40)   NOT NULL,
+    "subcategory_id" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" VARCHAR(50)   NOT NULL,
+    "subcategory" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "category" (
+    "category_id" VARCHAR(50)   NOT NULL,
+    "category" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contacts" ("contact_id_num");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "category" ("category_id");
+
+ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategory" ("subcategory_id");
+
+select * from campaign
+
+select * from category
+
+select * from subcategory
+
+select * from contacts
